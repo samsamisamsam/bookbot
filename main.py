f58487ea87fdf4f124d6e65ebc0fbe61b78b0add
samsamisamsam <samuel.larouche.cote@gmail.com>
2024-09-16 23:44:32 -0400

    added dict of character count

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 def main():
     book_file_path = "books/frankenstein.txt"
     text = get_text(book_file_path)
+
     word_count = get_word_count(text)
     print(f"This book contains {word_count} words!")
+
+    print(get_char_dict(text))
 
 def get_text(book_file_path):
     with open(book_file_path) as f:
@@ -10,5 +13,15 @@ def get_text(book_file_path):
 
 def get_word_count(text):
     return len(text.split())
+
+def get_char_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
 
 main()
