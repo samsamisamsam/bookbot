04925644036df23ad3b55e551736be30c7f28780
samsamisamsam <samuel.larouche.cote@gmail.com>
2024-09-22 23:55:13 -0400

    Bookbot completed

--- main.py
+++ main.py
@@ -1,27 +1,36 @@
-def main():
-    book_file_path = "books/frankenstein.txt"
-    text = get_text(book_file_path)
+def  main():
+    path_to_file = "books/frankenstein.txt"
+    text = get_text(path_to_file)
+    word_count = count_words(text)
+    char_dict = sort_char(count_char(text))
+    display_char_dict(path_to_file, word_count, char_dict)
 
-    word_count = get_word_count(text)
-    print(f"This book contains {word_count} words!")
-
-    print(get_char_dict(text))
-
-def get_text(book_file_path):
-    with open(book_file_path) as f:
+def get_text(path_to_file):
+    with open(path_to_file) as f:
         return f.read()
 
-def get_word_count(text):
+def count_words(text):
     return len(text.split())
 
-def get_char_dict(text):
-    chars = {}
-    for c in text:
-        lowered = c.lower()
-        if lowered in chars:
-            chars[lowered] += 1
-        else:
-            chars[lowered] = 1
-    return chars
+def count_char(text):
+    char = {}
+    lowered = text.lower()
+    for c in lowered:
+        if 96 < ord(c) < 123:
+            if c in char:
+                char[c] += 1
+            else:
+                char[c] = 1
+    return char
 
+def sort_char(char_dict):
+    return dict(sorted(char_dict.items(), key=lambda item: item[1],
+                       reverse=True))
+
+def display_char_dict(path, word_count, char_dict):
+    print(f"---Begin report of {path}")
+    print(f"{word_count} words found in the document.")
+    for char in char_dict:
+        print(f"The {char} character was found {char_dict[char]} times")
+    print("--- End report ---")
 main()
